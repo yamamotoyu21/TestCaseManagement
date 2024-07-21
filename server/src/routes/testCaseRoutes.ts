22c8d496846a16df5b13b3bc4c2476cc18f13e6c
yamamotoyu21 <yu.yamamoto@rakus-partners.co.jp>
2024-07-21 12:40:15 +0900

    update backend to create deleteTestCase and testCaseList

--- server/src/routes/testCaseRoutes.ts
+++ server/src/routes/testCaseRoutes.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import {
+  getTestCases,
   createTestCase,
   updateTestCase,
+  deleteTestCase,
 } from "../../controllers/testCaseController";
 
 const router = express.Router();
+
+router.get("/", getTestCases);
 router.post("/", createTestCase);
 router.put("/:id", updateTestCase);
+router.delete("/:id", deleteTestCase);
 
 export default router;
